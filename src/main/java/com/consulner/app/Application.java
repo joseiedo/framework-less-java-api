682cfba6015ae6d8e7d5a963f888dd24e0bf777f
José Iêdo <iedoduarte1@gmail.com>
2024-03-29 20:52:28 -0300

    feat: initial server runner

--- src/main/java/com/consulner/app/Application.java
+++ src/main/java/com/consulner/app/Application.java
@@ -1,4 +1,26 @@
 package com.consulner.app;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Application {
+
+    public static void main(String[] args) throws IOException {
+        int serverPort = 8000;
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        server.createContext("/api/hello", (exchange -> {
+            String response = "Hello!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream output = exchange.getResponseBody();
+            output.write(response.getBytes());
+            output.flush();
+            exchange.close();
+        }));
+        server.setExecutor(null);
+        server.start();
+    }
 }
