81f1fbadfaa8a6b2249fd0f5da2a76a0fe59a81c
José Iêdo <iedoduarte1@gmail.com>
2024-03-29 21:11:18 -0300

    feat: handle queryparams

--- src/main/java/com/consulner/app/Application.java
+++ src/main/java/com/consulner/app/Application.java
@@ -1,10 +1,13 @@
 package com.consulner.app;
 
+import com.consulner.app.api.ApiUtils;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
 
 public class Application {
 
@@ -15,7 +18,11 @@ public class Application {
         server.createContext("/api/hello", (exchange -> {
 
             if ("GET".equals(exchange.getRequestMethod())) {
-                String respText = "Hello!";
+                Map<String, List<String>> params = ApiUtils.splitQuery(exchange.getRequestURI().getRawQuery());
+                String noNameTxt = "Anonymous";
+                String name = params.getOrDefault("name", List.of(noNameTxt)).stream().findFirst().orElse(noNameTxt);
+                String respText = String.format("Hello %s!", name);
+
                 exchange.sendResponseHeaders(200, respText.getBytes().length);
                 OutputStream output = exchange.getResponseBody();
                 output.write(respText.getBytes());
