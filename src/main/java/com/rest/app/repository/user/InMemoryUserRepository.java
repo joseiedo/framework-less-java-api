adcbce74732c3c22777efc3b86d51aa344e0b442
José Iêdo <iedoduarte1@gmail.com>
2024-03-30 16:36:43 -0300

    feat: create InMemory repository

---
+++ src/main/java/com/rest/app/repository/user/InMemoryUserRepository.java
@@ -0,0 +1,27 @@
+package com.rest.app.repository.user;
+
+import com.rest.app.domain.user.NewUser;
+import com.rest.app.domain.user.User;
+import com.rest.app.domain.user.UserRepository;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryUserRepository implements UserRepository {
+
+    private static final Map USERS_STORE = new ConcurrentHashMap();
+
+    @Override
+    public String create(NewUser newUser) {
+        String id = UUID.randomUUID().toString();
+        User user = User.builder()
+            .id(id)
+            .login(newUser.getLogin())
+            .password(newUser.getPassword())
+            .build();
+        USERS_STORE.put(newUser.getLogin(), user);
+
+        return id;
+    }
+}
