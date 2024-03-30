2745a389c52431a66b9657a55517537ee271eeb9
José Iêdo <iedoduarte1@gmail.com>
2024-03-30 16:37:27 -0300

    feat: setup endpoints

---
+++ src/main/java/com/rest/app/Application.java
@@ -0,0 +1,56 @@
+package com.rest.app;
+
+import com.rest.app.controller.user.register.RegisterUserController;
+import com.rest.app.utils.ApiUtils;
+import com.sun.net.httpserver.BasicAuthenticator;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
+
+import static com.rest.app.config.UserBeansConfiguration.*;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        int serverPort = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+
+        RegisterUserController registerUserController = new RegisterUserController(getUserService(), getObjectMapper(),
+                getErrorHandler());
+
+        server.createContext("/api/users/register", registerUserController::handle);
+
+        HttpContext context = server.createContext("/api/hello", (exchange -> {
+
+            if ("GET".equals(exchange.getRequestMethod())) {
+                Map<String, List<String>> params = ApiUtils.splitQuery(exchange.getRequestURI().getRawQuery());
+                String noNameText = "Anonymous";
+                String name = params.getOrDefault("name", List.of(noNameText)).stream().findFirst().orElse(noNameText);
+                String respText = String.format("Hello %s!", name);
+                exchange.sendResponseHeaders(200, respText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(respText.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            }
+            exchange.close();
+        }));
+
+
+        context.setAuthenticator(new BasicAuthenticator("myrealm") {
+            @Override
+            public boolean checkCredentials(String user, String pwd) {
+                return user.equals("admin") && pwd.equals("admin");
+            }
+        });
+
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+}
